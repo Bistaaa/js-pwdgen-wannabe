68b0a372a57e3d9863a34cd26b0eb2d3e174baaa
Bistaaa <michele.bistarelli.mb@outlook.it>
2023-04-26 16:42:41 +0200

    fine lavoro

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 //dichiaro le variabili
+let userName, userSurname, userColor, password;
 
 //Chiedi all’utente il suo nome
+userName = prompt ("inserisci il tuo nome");
+console.log(userName);
 
 //Chiedi all’utente il suo cognome
+userSurname = prompt ("inserisci il tuo cognome");
+console.log(userSurname);
 
 //Chiedi all’utente il suo colore preferito
+userColor = prompt ("inserisci il tuo colore preferito");
+console.log(userColor);
 
-//scrivi password dell'utente
+//crea password dell'utente
+password = userName + userSurname + userColor;
+console.log(password);
+
+//comunica password all'utente
+document.getElementById("user_password").innerHTML = password;
